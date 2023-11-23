590f7c492c1cbb9ec0bd78a4f16fdc3de0bb59a2
tungduye <139089477+tungduye@users.noreply.github.com>
2023-11-23 22:09:51 +0700

    sexy.js

--- sexy.js
+++ sexy.js
@@ -8,9 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let playerCards = [
         "Bạn thích làm tình với một người khóc (điều khiến bạn khó chịu) mỗi khi họ đạt cực khoái hay người chửi bới bạn khi quan hệ? Bỏ phiếu cùng lúc, người thua phải uống 3 ly",
         "Bạn muốn thú cưng của mình ở trong phòng hay ra khỏi phòng khi bạn làm tình? Bỏ phiếu cùng lúc, bên thua phải uống 3 ly",
-        "Bài 3",
-        "Bài 4",
-        "Bài 5"
+        "Bạn thà không bao giờ tìm được tình yêu đích thực hay không bao giờ quan hệ tình dục nữa?",
+        "Bạn muốn quan hệ tình dục với một người nổi tiếng hay với người quyến rũ nhất mà bạn biết?",
+        "Bạn thà không thể cảm nhận được bất cứ điều gì về mặt tình dục mà được coi là người giỏi nhất, hay không biết gì trên giường và cảm nhận được mọi thứ?",
+        "Bạn muốn quan hệ tình dục ở nơi công cộng (giả sử có khoảng 20 người có thể nhìn thấy bạn) hay không quan hệ tình dục trong một năm?",
+        "Uống 2 ly nếu bạn đã từng cố tình gửi tin nhắn cho ai đó giả vờ như nó dành cho người khác"
+    
     ];
     let challengeCards = [
         "Hãy tặng 2 ly cho người quyến rũ nhất phòng. (Đúng, bạn rất gợi cảm, nhưng hãy chọn một người khác ngoài bạn.)",
@@ -19,10 +22,19 @@ document.addEventListener('DOMContentLoaded', () => {
         "Những việc chúng ta thường làm ngay sau khi ân ái, người đầu tiên hết ý tưởng, uống 2 ly.",
         "Hãy để người nào đó bạn chọn cho bạn một dấu hôn ở nơi bạn muốn. Từ chối nghĩa là 1 ly",
         "Người cuối cùng quan hệ tình dục cởi bỏ một mảnh quần áo",
-        "Mở shopee ai nhiều đơn hàng nhất thì uống",
-        "Chơi một trò chơi nhỏ: mỗi người phải hát một bản nhạc độc lập cùng một lúc trong 30 giây.",
-        "Thử thách một người khác trả lời mọi câu hỏi trong vòng 5 phút mà không được nói 'có' hoặc 'không' nếu thắng người bạn hỏi phải uống thua thì ngược lại."
-
+        "Uống 1 ly nếu thích bị cắn vào ngực",
+        "Nếu bạn đã từng ngủ với ai đó nhờ trò drinking game thì uống 2 ly",
+        "Đi quanh phòng và chọn người sẽ phải chạm vào háng bạn 2 ly cho mỗi lần từ chối.",
+        "Nếu bạn đã từng làm tình khi tắm thì hãy uống 3 ngụm. Nếu không hãy kể về chúng",
+        "Hãy chọn một bộ phận trên cơ thể bạn. Người chơi đầu tiên chạm vào phần này sẽ uống 1 ngụm",
+        "Uống 1 lần nếu bạn đã từng hẹn hò với người yêu cũ của bạn mình",
+        "Nếu bạn đã từng quan hệ tình dục với người đã có gia đình thì hãy uống 2 ly.",
+        "Người quan hệ tình dục nhiều nhất nhờ trang web hẹn hò uống 2 ly",
+        "Uống 1 ly nếu bạn đã từng bị tổn thương vì đối phương dùng răng",
+        "Xung quanh phòng, hãy kể tên từng mối quan hệ hoặc mối quan hệ cũ của bạn. Người đầu tiên hết tên, uống 2 ly.",
+        "Mọi người cởi 1 cái ra đi! 1 người không thì tất cả đồng khởi nào!",
+        "Nếu bạn đã quan hệ tình dục với người chơi khác trong phòng, hãy uống 2 ly",
+        "Massage đầu cho người bên cạnh hoặc uống 3 ly"
     ];
     let currentChallenge = null;
 
@@ -58,9 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
         playerCards = [
             "Bạn thích làm tình với một người khóc (điều khiến bạn khó chịu) mỗi khi họ đạt cực khoái hay người chửi bới bạn khi quan hệ? Bỏ phiếu cùng lúc, người thua phải uống 3 ly",
             "Bạn muốn thú cưng của mình ở trong phòng hay ra khỏi phòng khi bạn làm tình? Bỏ phiếu cùng lúc, bên thua phải uống 3 ly",
-            "Bài 3",
-            "Bài 4",
-            "Bài 5"
+            "Bạn thà không bao giờ tìm được tình yêu đích thực hay không bao giờ quan hệ tình dục nữa?",
+            "Bạn muốn quan hệ tình dục với một người nổi tiếng hay với người quyến rũ nhất mà bạn biết?",
+            "Bạn thà không thể cảm nhận được bất cứ điều gì về mặt tình dục mà được coi là người giỏi nhất, hay không biết gì trên giường và cảm nhận được mọi thứ?",
+            "Bạn muốn quan hệ tình dục ở nơi công cộng (giả sử có khoảng 20 người có thể nhìn thấy bạn) hay không quan hệ tình dục trong một năm?",
+            "Uống 2 ly nếu bạn đã từng cố tình gửi tin nhắn cho ai đó giả vờ như nó dành cho người khác"
+        
         ];
         challengeCards = [
             "Hãy tặng 2 ly cho người quyến rũ nhất phòng. (Đúng, bạn rất gợi cảm, nhưng hãy chọn một người khác ngoài bạn.)",
@@ -69,9 +84,19 @@ document.addEventListener('DOMContentLoaded', () => {
             "Những việc chúng ta thường làm ngay sau khi ân ái, người đầu tiên hết ý tưởng, uống 2 ly.",
             "Hãy để người nào đó bạn chọn cho bạn một dấu hôn ở nơi bạn muốn. Từ chối nghĩa là 1 ly",
             "Người cuối cùng quan hệ tình dục cởi bỏ một mảnh quần áo",
-            "Mở shopee ai nhiều đơn hàng nhất thì uống",
-            "Chơi một trò chơi nhỏ: mỗi người phải hát một bản nhạc độc lập cùng một lúc trong 30 giây.",
-            "Thử thách một người khác trả lời mọi câu hỏi trong vòng 5 phút mà không được nói 'có' hoặc 'không' nếu thắng người bạn hỏi phải uống thua thì ngược lại."
+            "Uống 1 ly nếu thích bị cắn vào ngực",
+            "Nếu bạn đã từng ngủ với ai đó nhờ trò drinking game thì uống 2 ly",
+            "Đi quanh phòng và chọn người sẽ phải chạm vào háng bạn 2 ly cho mỗi lần từ chối.",
+            "Nếu bạn đã từng làm tình khi tắm thì hãy uống 3 ngụm. Nếu không hãy kể về chúng",
+            "Hãy chọn một bộ phận trên cơ thể bạn. Người chơi đầu tiên chạm vào phần này sẽ uống 1 ngụm",
+            "Uống 1 lần nếu bạn đã từng hẹn hò với người yêu cũ của bạn mình",
+            "Nếu bạn đã từng quan hệ tình dục với người đã có gia đình thì hãy uống 2 ly.",
+            "Người quan hệ tình dục nhiều nhất nhờ trang web hẹn hò uống 2 ly",
+            "Uống 1 ly nếu bạn đã từng bị tổn thương vì đối phương dùng răng",
+            "Xung quanh phòng, hãy kể tên từng mối quan hệ hoặc mối quan hệ cũ của bạn. Người đầu tiên hết tên, uống 2 ly.",
+            "Mọi người cởi 1 cái ra đi! 1 người không thì tất cả đồng khởi nào!",
+            "Nếu bạn đã quan hệ tình dục với người chơi khác trong phòng, hãy uống 2 ly",
+            "Massage đầu cho người bên cạnh hoặc uống 3 ly"
 
         ];
         playerHand.innerHTML = "";
